55805a3dd57e104b891fc95dba089096b6f0072f
dgonz <dgonzalezhernandez428@gmail.com>
2026-03-05 23:23:46 -0600

    ultima modificacion

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -4,7 +4,10 @@ from app.database import get_db
 from app.models.user_model import User
 from app.schemas.user_schema import UserRegister
 from app.utils.security import hash_password
-
+from app.schemas.user_schema import UserResponse, UserUpdate
+from app.schemas.user_schema import LoginRequest
+from app.utils.security import verify_password
+from app.utils.jwt_handler import create_access_token
 router = APIRouter()
 
 @router.post("/register")
@@ -29,3 +32,25 @@ def register(user: UserRegister, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return {"message": "Usuario registrado correctamente"}
+
+# ENPOINT PARA LOGIN
+@router.post("/login")
+def login(data: LoginRequest, db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.email == data.email).first()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Credenciales incorrectas")
+
+    if not verify_password(data.password, user.password):
+        raise HTTPException(status_code=401, detail="Credenciales incorrectas")
+
+    token = create_access_token({
+        "sub": str(user.id),
+        "email": user.email
+    })
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -40,24 +40,3 @@ def update_user(user_id: int, user_data: UserUpdate, db: Session = Depends(get_d
 
     return user
 
-# ENPOINT PARA LOGIN
-@router.post("/login")
-def login(data: LoginRequest, db: Session = Depends(get_db)):
-
-    user = db.query(User).filter(User.email == data.email).first()
-
-    if not user:
-        raise HTTPException(status_code=401, detail="Credenciales incorrectas")
-
-    if not verify_password(data.password, user.password):
-        raise HTTPException(status_code=401, detail="Credenciales incorrectas")
-
-    token = create_access_token({
-        "sub": str(user.id),
-        "email": user.email
-    })
-
-    return {
-        "access_token": token,
-        "token_type": "bearer"
-    }
